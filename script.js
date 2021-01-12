a3ba71ed2f31c25c7d9a69f8c00df2b843d70f6d
Clement Moreau <moreauc@saintjo.org>
2021-01-12 15:26:34 +0100

    Pb JavaScript

---
+++ script.js
@@ -0,0 +1,33 @@
+
+alert('Fichier JS bien appelé');
+
+//function connection(){
+ //   alert('connection impossible');
+//}
+
+
+if(document.getElementById('formulaire_contact')){
+    alert('ok');
+    var email = document.getElementById("email");
+//    var mdp = document.getElementById("mdp").value;
+//    var msg = document.getElementById("msg").value;
+    alert(email.value);
+//    alert(mdp);
+//    alert(msg);
+    
+//    if(email || mdp || msg ){
+//        alert("tout rempli");
+//       
+//       }
+    
+}
+
+//function formulaire(){
+//    alert('ok');
+//    
+////   if (MotDepasse OR email OR Message == empty ){
+////        alert('Veuillez remplir tout les champs');
+////    }else{
+////        alert("c'est good");
+////    }
+//}
