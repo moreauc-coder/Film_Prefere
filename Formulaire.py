2006b6d0e590e9bcf93260d554acdc5f30016075
kevin-ccpg <hennionke@cc-parthenay-gatine.fr>
2021-01-19 14:25:54 +0100

    Restructuration pour module CGI

--- Formulaire.py
+++ Formulaire.py
@@ -14,9 +14,6 @@ pseudo = ""
 message = ""
 
 
-
-
-
 print("Content-type: text/html; charset=utf-8\n")
 html = """
 <!DOCTYPE html>
@@ -28,7 +25,7 @@ html = """
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="/static/src/css/style.css">
     <script src="https://kit.fontawesome.com/d7d8afdd19.js" crossorigin="anonymous"></script>
     <title>Le petit ciné</title>
     
@@ -140,7 +137,7 @@ html="""
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="/static/src/javascript/script.js"></script>
 
 </body>
 </html>
